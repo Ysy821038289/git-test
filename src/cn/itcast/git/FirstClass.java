50c73c49aec577c167c77e1911d4c6418d610da8
git <git@itcast.cn>
2019-06-10 17:10:11 +0800

    把本地工程添加到get

---
+++ src/cn/itcast/git/FirstClass.java
@@ -0,0 +1,11 @@
+package cn.itcast.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hellow,git");
+    }
+    public void test(){
+        int i =10;
+        i+=10;
+    }
+}
